03f339710aac14a9f67510af25410bd378696c11
juliekaam <nestlyguiabu@gmail.com>
2023-07-12 15:24:04 +0200

    password with bonus

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -3,21 +3,55 @@ package org.lessons.java.security;
 import java.util.Scanner;
 public class PasswordGenerator {
     public static void main(String[] args) {
-        //Dichiarazione e assegnazione delle variabile
 
-        String name="kaam";
-        String surname="julienne";
-        String favoriteColor="white";
-        int dayOfBirth=3;
-        int monthOfBirth=10;
-        int yearOfBirth=1997;
-        String birthInfo= dayOfBirth +"/" + monthOfBirth +"/" + yearOfBirth ;
-        int sumBirthInfo= dayOfBirth+monthOfBirth+yearOfBirth;
-        String password=name  + surname + favoriteColor + sumBirthInfo ;
+        // dichiaro che voglio uno scanner da tastiera
+        Scanner scanner = new Scanner(System.in);
 
+        //declaring variables
+
+        String name;
+        String surname;
+        String favoriteColor;
+        int dayOfBirth;
+        int monthOfBirth;
+        int yearOfBirth;
+        int sumBirthInfo;
+        String birthInfo ;
+
+
+
+        // getting information from the printer
+
+        System.out.println("inter your name:");
+        name = scanner.nextLine();
+        System.out.println("inter your surname:");
+        surname = scanner.nextLine();
+        System.out.println("what is your favorite color:");
+        favoriteColor = scanner.nextLine();
+
+        //asking day of birth
+
+        System.out.println("inter your day of birth:");
+        dayOfBirth = scanner.nextInt();
+
+        //asking month of birth
+
+        System.out.println("inter your month of birth:");
+        monthOfBirth = scanner.nextInt();
+
+        //asking year of birth
+
+        System.out.println("inter your year of birth:");
+        yearOfBirth = scanner.nextInt();
+
+        // print birth information
+        birthInfo=dayOfBirth + "/" + monthOfBirth + "/" + yearOfBirth;
+        System.out.println("your birth information: " + birthInfo);
         //stampa a video
-        System.out.println("la somma è:" +sumBirthInfo);
-        System.out.println("la tua password è:" +password);
+        sumBirthInfo = dayOfBirth + monthOfBirth + yearOfBirth;
 
+        System.out.println("the sum is:" + sumBirthInfo);
+        String password = name + surname + favoriteColor + sumBirthInfo;
+        System.out.println("your password is:" + password);
     }
 }
